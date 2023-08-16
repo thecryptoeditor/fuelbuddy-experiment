51ef047658ecc35b20779e8b0ecc304add7d4e13
jatin1213 <jatin@shyplite.com>
2023-08-17 02:55:29 +0530

    env file for firebase.

--- src/main.js
+++ src/main.js
@@ -41,6 +41,19 @@ axios.interceptors.response.use(
     }
 );
 
+
+// Firebsae intigration
+// const config = {
+//     apiKey: process.env.VUE_APP_apiKey,
+//     authDomain: process.env.VUE_APP_authDomain,
+//     databaseURL: process.env.VUE_APP_databaseURL,
+//     projectId: process.env.VUE_APP_projectId,
+//     storageBucket: process.env.VUE_APP_storageBucket,
+//     messagingSenderId: process.env.VUE_APP_messagingSenderId
+// };
+
+
+
 axios.defaults.withCredentials = true;
 
 // API base URL for Application
