99b2fedc341c787b5bf87bb0d83ffddc834c0852
jatin1213 <jatin@shyplite.com>
2023-08-17 02:12:20 +0530

    Tailwind loading issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+	],
 }
-
