1848b54ebdd6c46c854f13c99e67bee622000e59
jatin1213 <jatin@shyplite.com>
2023-08-18 18:12:52 +0530

    Composition API used throughout the APP.

--- src/components/button.vue
+++ src/components/button.vue
@@ -3,15 +3,15 @@
         class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline"
         @click="$emit('click')"
     >
-        {{ label }}
+        {{ props.label }}
     </button>
 </template>
   
-<script>
-    export default {
-        props: {
-            label: String,
-        },
-    }
+<script setup>
+
+    const props = defineProps({
+        label: String,
+    })
+
 </script>
   
--- src/components/card.vue
+++ src/components/card.vue
@@ -6,9 +6,7 @@
     </div>
 </template>
   
-<script>
-    export default {
-        name: "Card",
-    }
+<script setup>
+
 </script>
   
